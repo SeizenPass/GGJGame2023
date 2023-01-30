151af72961b5ee5c8a5743167c7d92a3a229c53e
Amiran Kurman <amiran.k@aloteq.com>
2023-01-30 22:40:58 +0600

    base

---
+++ Assets/Project/Scripts/Core/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace Project.Core
+{
+    public class Constants
+    {
+        public const string ScriptableObjectPrefix = "Project" + Slash;
+        public const string Slash = "/";
+    }
+}
